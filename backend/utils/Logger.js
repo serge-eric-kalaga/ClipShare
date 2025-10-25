960923c42f7e72eec1bef133603b2788b759e9ee
ekalaga <kalagaserge4@gmail.com>
2025-10-25 23:22:01 +0000

    fix: corriger la configuration du transporteur de pino pour gérer l'environnement de production

--- backend/utils/Logger.js
+++ backend/utils/Logger.js
@@ -2,10 +2,10 @@ const logger = require("pino");
 // const dayjs = require("dayjs");
 
 const transport = logger.transport({
-    target: 'pino-pretty',
-    options: { colorize: true }
-  })
+  target: process.env.NODE_ENV === "production" ? "pino/file" : "pino-pretty",
+  options: { colorize: true }
+})
 
-const log = logger({ level: process.env.LOG_LEVEL ,}, transport)
+const log = logger({ level: process.env.LOG_LEVEL, }, transport)
 
 module.exports = log;
